8800494331687166bb8264d64f8f34a1eaad937e
4nc3str4l <muri1216>
2015-02-02 21:30:38 +0100

    Funny efect

--- LD_30/Assets/Ressources/Scripts/Filter.cs
+++ LD_30/Assets/Ressources/Scripts/Filter.cs
@@ -5,17 +5,20 @@ public class Filter : MonoBehaviour {
 
 	// Use this for initialization
 
-
-
+	public float shotRate = 0.1f;
+	public float nextShot;
 	void Start () {
-	
-		RenderSettings.ambientLight = Color.blue;
-        
+		this.nextShot = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log ("updating");
+		if (Time.time > this.nextShot) {
+			RenderSettings.ambientLight = new Color (0f, 0f ,Random.Range (0.1f,1f) , 1f);
+			this.nextShot = Time.time + shotRate;
+			Debug.Log("changing color");
+		}
 	}
 
 
@@ -33,6 +36,22 @@ public class Filter : MonoBehaviour {
 		return text;
 
 
+	}
+
+	public static Texture2D createNewTexture(int w , int h){
+		Color[] pixels = new Color[w * h];  
+		
+		for(int i = 0;i<pixels.Length;i++){
+			pixels[i] = new Color (0f, 0f ,Random.Range (0.1f,1f) , 0.1f);
+		}
+		
+		Texture2D text = new Texture2D(w,h);
+		
+		text.SetPixels(pixels);
+		
+		return text;
+		
+		
 	}
 
 	void OnGUI(){
